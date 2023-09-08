58e847000eb91522cd9d42328ca66681c02cebef
akarahdev <118059229+akarahdev@users.noreply.github.com>
2023-09-08 07:32:14 -0400

    uhhhhhhhhh

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,8 @@
-import bytecode.Emitter
-import bytecode.Pretransformer
 import lexer.Lexer
 import parser.Parser
 import parser.ParserError
 import parser.Value
-import server.*
-import server.core.startupServer
 import server.interpreter.Interpreter
-import server.interpreter.disassemble
-import server.interpreter.transform
 import java.io.File
 import java.nio.ByteBuffer
 import java.time.LocalDate
@@ -36,18 +30,6 @@ fun main(args: Array<String>) {
         val time1 = LocalDate.now()
         val ast = parser.parseAll()
         Logger.trace(ast)
-        val flattened = Pretransformer(ast).transform()
-        println(flattened)
-//        val emitter = Emitter()
-//        var bytes = emitter.startEmitting(ast)
-//        bytes = bytes.position(0)
-//        val interpreter = Interpreter(bytes)
-//        interpreter.transform()
-//        globalInterpreter = interpreter
-//        Logger.trace("blockmap:")
-//        globalInterpreter.printBlockMap()
-//        globalInterpreter.disassemble()
-//        startupServer()
     } catch(e: ParserError) {
         println(e.emit())
     }
